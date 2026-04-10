130b23a24ba9427c46b1fa9aec9bdd273d03f1e6
sheleek1n <kenestaniel@gmail.com>
2026-04-10 17:29:26 +0800

    chore: gitignore, readme update, and electron dev setup

---
+++ electron/main.cjs
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1024,
+    minHeight: 700,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    title: "Chelsy's Burger POS",
+    autoHideMenuBar: true,
+  })
+
+  // Load from Vite dev server
+  win.loadURL('http://localhost:5173')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
